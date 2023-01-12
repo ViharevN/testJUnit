d1db6c37bd791fa037856e8d65f218cacd5557b6
ViharevN <swix_off@mail.ru>
2023-01-12 22:43:50 +0300

    commit

---
+++ src/test/java/me/viharev/junit/JUnitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.viharev.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
